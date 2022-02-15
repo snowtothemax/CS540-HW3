7f61a5f98f24c95320c3926aa11f111196c5516d
Max J <mkjohnson9@wisc.edu>
2022-02-14 20:43:23 -0600

    fixed files

--- hw3.py
+++ hw3.py
@@ -68,3 +68,10 @@ def display_image(orig, proj):
 
     plt.show()
 
+x = load_and_center_dataset('YaleB_32x32.npy')
+S = get_covariance(x)
+Lambda, U = get_eig(S, 2)
+projection = project_image(x[0], U)
+display_image(x[0], projection)
+
+
